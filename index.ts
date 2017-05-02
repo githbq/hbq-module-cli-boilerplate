23ca029a01b30faf01d852d640641bab7b6ad05b
hbq <hbqhbq123@qq.com>
2017-05-02 23:10:24 +0800

    init

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
 #!/usr/bin/env node --harmony
+
 import * as path from 'path'
 
-console.log('path--->',path.join('adf','/fsd/fsd/f/sdf/sd')) 
+console.log('path--->', path.join('adf', '/fsd/fsd/f/sdf/sd'))
 
 let yargs = require('yargs')
 let argv = yargs
     .command('init', '初始化', {
-            template: {
-                alias: ['t', 'templateName'],
-                default: '',
-                describe: '模板'
-            },
-            name: {
-                alias: ['n', 'projectName'],
-                default: '',
-                describe: '项目名称'
-            }
+        template: {
+            alias: ['t', 'templateName'],
+            default: '',
+            describe: '模板'
         },
-        (argv) => {
-            require('../command/init').start(argv)
-        })
-    .help() 
+        name: {
+            alias: ['n', 'projectName'],
+            default: '',
+            describe: '项目名称'
+        }
+    },
+    (argv) => {
+        require('../command/init').start(argv)
+    })
+    .help()
     .argv
 
 if (!argv._.length) {
