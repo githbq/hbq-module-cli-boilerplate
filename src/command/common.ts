38c96f77c535cb47c798e5fe14466868b85a8835
hbq <hbqhbq123@qq.com>
2017-05-05 00:32:59 +0800

    add exec

--- src/command/common.ts
+++ src/command/common.ts
@@ -2,11 +2,13 @@ import * as  ioHelper from 'io-helper'
 import * as prompt from 'prompt-promise'
 import * as  chalk from 'chalk'
 import * as _ from 'lodash'
+import spawn from 'spawn-helper'
 /**
  * 公共属性及方法
  */
 export default {
-    rootPath: ioHelper.pathTool.join(__dirname, '..'),
+    cwd: process.cwd(),
+    rootPath: ioHelper.pathTool.join(__dirname, '..', '..'),
     async prompt(describe) {
         let value = await prompt(describe)
         return _.trim(value)
@@ -20,5 +22,8 @@ export default {
     async confirm(describe) {
         let result = await prompt(`${describe}(y/n):`)
         return result.toLowerCase().indexOf('y') != -1
+    },
+    async exec(cmd: string, opt?: any) {
+        return spawn.exec(cmd, opt)
     }
 }
