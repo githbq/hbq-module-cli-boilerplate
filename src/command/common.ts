f38555492cc10821d771b0eef71f627d4a7c80da
hubiqing01 <hubiqing01@58ganji.com>
2017-05-05 12:33:56 +0800

    upgrade prompt-promise2

--- src/command/common.ts
+++ src/command/common.ts
@@ -1,5 +1,5 @@
 import * as  ioHelper from 'io-helper'
-import prompt from 'prompt-promise2'
+import { prompt } from 'prompt-promise2'
 import * as  chalk from 'chalk'
 import * as _ from 'lodash'
 import spawn from 'spawn-helper'
