08db3615d93e0c617c123053abf3bbad52388a60
hbq <hbqhbq123@qq.com>
2017-05-07 21:54:39 +0800

    common/index

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
 #!/usr/bin/env node --harmony
 
-import * as  yargs from 'yargs'
-import startCommand from './command/start'
-
-let argv = yargs
-    .command('init', '初始化', {
-        template: {
-            alias: ['t', 'templateName'],
-            default: '',
-            describe: '模板'
-        },
-        name: {
-            alias: ['n', 'projectName'],
-            default: '',
-            describe: '项目名称'
-        }
-    },
-    (argv) => {
-        startCommand.start(argv)
-    })
-    .help()
-    .argv
-
-if (!argv._.length) {
-    yargs.showHelp()
-}
+import command from './command'
+command.start()
