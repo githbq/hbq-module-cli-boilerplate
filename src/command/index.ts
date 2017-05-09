7de46f04418ee225f2089558be1b709be946f560
hubiqing01 <hubiqing01@58ganji.com>
2017-05-09 11:46:17 +0800

    test

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,23 +1,10 @@
 import * as requireHelper from 'require-helper'
 import * as  yargs from 'yargs'
-/**
- * 加载当前目录下的每一个文件，除了common文件 
- * 将每一个命令文件里面的 command属性注册到yargs
- */
 export default {
-    getCommands(cb?) {
-        return requireHelper.requireDir(__dirname, cb, () => {
-
-        })
-    },
     start() {
-
-
-        this.getCommands().forEach(({ name, result }) => {
+        const commands = requireHelper.requireDir(__dirname)
+        commands.forEach(({ name, result }) => {
             if (name !== 'common' && result.command) {
-                /**
-                 * 每一个命令文件必须有command参数 用来定义命令参数
-                 */
                 yargs.command.apply(null, result.command).help()
             }
         })
