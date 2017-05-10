8fd85db40e07556f7d840ed90598953b9854f06e
hubiqing01 <hubiqing01@58ganji.com>
2017-05-10 15:13:12 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-require('./command').default.start()
-console.log('hello xxxx')
+import * as requireDir from 'require-dir'
+import * as  yargs from 'yargs' 
+export function start() {
+    const commands = requireDir('./command')
+    Object.keys(commands).forEach(key => {
+        const result = commands[key].default
+        if (result && !(/^(common|index|_)/.test(key))) {
+            yargs.command.apply(null, result.command.slice(0, 3).concat((argv) => {
+                result.start(argv)
+            })).help()
+        }
+    })
+    let argv = yargs.version().argv
+    if (!argv._.length) {
+        yargs.showHelp()
+    }
+    return { argv, yargs }
+}
+
+start()
