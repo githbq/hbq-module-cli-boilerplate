8549476c790b6cc490c132a11c7529631dab2a27
hubiqing01 <hubiqing01@58ganji.com>
2017-05-12 14:20:16 +0800

    io

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -1,18 +1,22 @@
 import * as pathTool from 'path'
 import * as fs from 'fs-extra-promise'
-import * as _ from 'lodash'
 import { rootPath, cwd } from './consts'
 import { stringify } from '.'
 import walkFolderTree from './walkFolderTree'
+import * as _ from 'lodash'
 export const io = {
     pathTool,
-    read(path) {
-        path = pathTool.join.apply(null, [].concat(path))
-        return fs.readFileAsync(path, 'utf8')
-    },
-    write(path, content, options: any = { fromRoot: false, fromCwd: false }) {
+    resolveOptions(path: string, options: any = { fromRoot: false, fromCwd: false }) {
         path = pathTool.join.apply(null, [].concat(path))
         path = pathTool.join.apply(null, (options.fromRoot ? [rootPath] : options.fromCwd ? [cwd] : []).concat(path))//考虑多路径处理
+        return path
+    },
+    read(path: string, options: any = { fromRoot: false, fromCwd: false }) {
+        path = this.resolveOptions(path, options)
+        return fs.readFileAsync(path, 'utf8')
+    },
+    write(path: string, content, options: any = { fromRoot: false, fromCwd: false }) {
+        path = this.resolveOptions(path, options)
         //对对象进行 美化格式处理
         content = _.isObject(content) ? stringify(content) : content
         return fs.outputFileAsync(path, content)
