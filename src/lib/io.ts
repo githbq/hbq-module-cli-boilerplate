ef4add9a4c726c3c697143caa1900954bcad37ed
hubiqing <hubiqing01@58ganji.com>
2017-08-03 17:13:10 +0800

    io

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -14,32 +14,32 @@ export const io = {
      * @param path 路径　
      * @param options 参数
      */
-    resolveOptions(path: string, options: any = { fromRoot: false, fromCwd: false }) {
+    resolveOptions(path: string | Array<string>, options: any = { fromRoot: false, fromCwd: false }) {
         path = pathTool.join.apply(null, [].concat(path))
         // 考虑多路径处理
         path = pathTool.join.apply(null, (options.fromRoot ? [rootPath] : options.fromCwd ? [cwd] : []).concat(path))
         return path
     },
-    read(path: string, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.readFileAsync(path, 'utf8')
+    read(path: string | Array<string>, options: any = { fromRoot: false, fromCwd: false }) {
+        const newPath = this.resolveOptions(path, options)
+        return fs.readFileAsync(newPath)
     },
-    readJson(path: string, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.readJsonAsync(path, 'utf8')
+    readJson(path: string | Array<string>, options: any = { fromRoot: false, fromCwd: false }) {
+        const newPath = this.resolveOptions(path, options)
+        return fs.readJsonAsync(newPath)
     },
-    write(path: string, content, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
+    write(path: string | Array<string>, content, options: any = { fromRoot: false, fromCwd: false }) {
+        const newPath = this.resolveOptions(path, options)
         // 对对象进行 美化格式处理
         content = _.isObject(content) ? stringify(content) : content
-        return fs.outputFileAsync(path, content)
+        return fs.outputFileAsync(newPath, content)
     },
-    delete(path, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.removeAsync(path)
+    delete(path: string | Array<string>, options: any = { fromRoot: false, fromCwd: false }) {
+        const newPath = this.resolveOptions(path, options)
+        return fs.removeAsync(newPath)
     },
-    exists(path, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.existsAsync(path)
+    exists(path: string | Array<string>, options: any = { fromRoot: false, fromCwd: false }) {
+        const newPath = this.resolveOptions(path, options)
+        return fs.existsAsync(newPath)
     },
 }
