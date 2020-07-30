5a425225695f3b622638804179534a8a9cb804e3
hubiqing <hbqhbq123@qq.com>
2020-07-30 14:37:24 +0800

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 import * as requireDir from 'require-dir'
 import * as  yargs from 'yargs'
-import { exit } from './lib'
+import { exit, consoleColor } from './lib'
+import * as  momentHelper from 'moment-helper'
 export function start() {
-    const commands = requireDir('./command')
-    Object.keys(commands).forEach(key => {
-        const result = commands[key].default
-        if (result && !(/^(common|index|_)/.test(key))) {
-            yargs.command.apply(null, result.command.slice(0, 3).concat(async (argv) => {
-                try {
-                    await result.start(argv)
-                } catch (e) {
-                    console.error('发生错误', e.message)
-                    console.error(e.stack)
-                }
-                exit()
-            })).help()
+  const commands = requireDir('./command', { recurse: true })
+
+  consoleColor.yellow(' 操作时间：' + momentHelper.get() + '\n')
+  Object.keys(commands).forEach(key => {
+    const result = (commands[key].index || commands[key]).default
+    if (result && !(/^(common|index|_)/.test(key)) && result.start) {
+      yargs.command.apply(null, result.command.slice(0, 3).concat(async (argv) => {
+        try {
+          await result.start(argv)
+        } catch (e) {
+          consoleColor.red(`发生错误${e.message}`)
+          consoleColor.red(e.stack)
         }
-    })
-    let argv = yargs.version().argv
-    if (!argv._.length) {
-        yargs.showHelp()
+        exit()
+      })).help()
     }
-    return { argv, yargs }
+  })
+  let argv = yargs.version().argv
+  if (!argv._.length) {
+    yargs.showHelp()
+  }
+  return { argv, yargs }
 }
 
 start()
