96284bad9e24d653784e9ee815b63c8583b58380
hubiqing <hubiqing@jd.com>
2021-03-11 14:03:56 +0800

    template

--- src/lib/template.ts
+++ src/lib/template.ts
@@ -2,15 +2,20 @@ import { template } from 'lodash'
 import * as fs from 'fs-extra'
 import * as pathTool from 'path'
 
-export const templateTransfrom = async (dir, config) => { 
+export const templateTransfrom = async (dir, config) => {
     const paths = Object.keys(config)
     for (let path of paths) {
         const pathData = config[path]
-        const fileAbsolutePath = pathTool.join(dir, ...path.split('/')) 
-        if (await fs.pathExists(fileAbsolutePath)) {
-            const content = await fs.readFile(fileAbsolutePath, 'utf8') 
-            const newContent = template(content)(pathData) 
-            await fs.outputFile(fileAbsolutePath, newContent)
+        const fileAbsolutePath = pathTool.join(dir, ...path.split('/'))
+        try {
+            if (await fs.pathExists(fileAbsolutePath)) {
+                const content = await fs.readFile(fileAbsolutePath, 'utf8')
+                const newContent = template(content)(pathData)
+                await fs.outputFile(fileAbsolutePath, newContent)
+            }
+        } catch (err) {
+            console.error(`文件模板: ${fileAbsolutePath} 转换异常`)
+            console.error(err)
         }
     }
 }
