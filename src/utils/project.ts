ffe5de6873403238cde265c6ca34fe9c50bc8712
hubiqing <hubiqing@jd.com>
2021-03-17 15:52:51 +0800

    init

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,19 +1,28 @@
 
 import { exec } from './other'
-import { consoleColor } from './consoleColor' 
+import { consoleColor } from './consoleColor'
+import * as pathTool from 'path'
+import * as fs from 'fs-extra'
 
 export const initProject = async (cwd) => {
     const cmds = [
         'git init',
         'git add . && git commit -am "init"',
-        'code .'
-    ] 
+        'code .',
+        async () => {
+            const yarnFeature = await fs.pathExists(pathTool.join(cwd, 'yarn.lock'))
+            return `${yarnFeature ? 'yarn' : 'npm'} install`
+        }
+    ]
     for (let cmd of cmds) {
         try {
+            if (typeof cmd === 'function') {
+                cmd = await cmd()
+            }
             consoleColor.start(cmd)
             await exec(cmd, { cwd })
         } catch (e) {
             consoleColor.error(e)
         }
-    } 
+    }
 }
