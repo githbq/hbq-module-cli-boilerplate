8c5945afd8a1c38412d4a369b3914f2586bbc030
hubiqing <hubiqing@jd.com>
2021-03-17 15:58:04 +0800

    test

--- run.js
+++ run.js
@@ -1,4 +1,5 @@
  require('ts-node/register') 
+ process.chdir('C:\\Users\\hubiqing\\Desktop\\新建文件夹')
  require('./src')
      //tsconfig 帮助文档地址
      //https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/tsconfig.json.html
